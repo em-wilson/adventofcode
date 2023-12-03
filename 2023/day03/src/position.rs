2dce3a0291cc8142deb877af9ba3bb5b5cc51959
Em Wilson <em.wilson@7shifts.com>
2023-12-03 15:13:11 -0400

    2023 day 3

---
+++ 2023/day03/src/position.rs
@@ -0,0 +1,35 @@
+#[derive(Clone,Copy,Debug,PartialEq)]
+pub struct Pos {
+    pub x: i32, 
+    pub y: i32,
+}
+
+impl Pos {
+    pub fn from(x:i32, y:i32) -> Pos {
+        return Pos{x:x,y:y};
+    }
+
+    pub fn is_adjacent_to(&self, pos:Pos) -> bool {
+        let compare = *self;
+        return pos == compare
+        || pos == Pos::from(compare.x-1, compare.y-1)
+        || pos == Pos::from(compare.x, compare.y-1)
+        || pos == Pos::from(compare.x+1, compare.y-1)
+        || pos == Pos::from(compare.x-1, compare.y)
+        || pos == Pos::from(compare.x+1, compare.y)
+        || pos == Pos::from(compare.x-1, compare.y+1)
+        || pos == Pos::from(compare.x, compare.y+1)
+        || pos == Pos::from(compare.x+1, compare.y+1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Pos;
+
+    #[test]
+    fn test_position_is_adjacent() {
+        assert_eq!(true, Pos::from(3,3).is_adjacent_to(Pos::from(2,2)));
+        assert_eq!(false, Pos::from(3,3).is_adjacent_to(Pos::from(8,6)));
+    }
+}
